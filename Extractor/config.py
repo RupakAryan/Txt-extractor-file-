9923e2651d95e3aa0a9a0bc36ffcbc26a94af96f
RupakAryan <157547044+RupakAryan@users.noreply.github.com>
2025-10-07 09:51:22 +0530

    Update config.py

--- Extractor/config.py
+++ Extractor/config.py
@@ -39,13 +39,13 @@ THUMB_URL = os.environ.get("THUMB_URL", "https://img.freepik.com/premium-photo/b
 
 
 # # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "22746239"))
-# API_HASH = os.environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "7547829346:AAGyfvOu47EciNhC7NUGSDEDFuBaetYYusw")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "MassRPBot")
+# API_ID = int(os.environ.get("API_ID", "29978901"))
+# API_HASH = os.environ.get("API_HASH", "500fc876c5356cf04ed3698912dc2edf")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "8400419495:AAHPVhXaN4uKeiCGjvNGhic5S1SZWdH2YFc")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "Txtextractorbotbot")
 # OWNER_ID = int(os.environ.get("OWNER_ID", "7463601722"))
 # SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7463601722").split()))
-# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002601604234"))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003068385283"))
 # MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002601604234"))
 # THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
